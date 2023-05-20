c78b32b93866b2d281f40675468fc49d80d556a0
d.ragozin <dmtryragozin@gmail.com>
2023-05-20 16:21:04 +0300

    Feat: Linting configuration

---
+++ app/alarms.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const Alarms: React.FC = () => {
+  return (
+    <View>
+      <Text>Alarms</Text>
+    </View>
+  );
+};
+
+export default Alarms;
